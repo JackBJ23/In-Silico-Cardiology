758ee42d789f35092e3a9968af1ab9a3fec18cb7
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2023-09-11 20:46:10 +0200

    Create predictions.py

---
+++ predictions.py
@@ -0,0 +1,3 @@
+'''
+Program makes predictions of hypertension, stenosis, etc. based on the circuit representations of patients. All results are put together in a table at the end of the program.
+'''
