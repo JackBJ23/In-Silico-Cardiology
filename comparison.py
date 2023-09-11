4510d9f09917a2b57d5f1aa112233883c32e36a3
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2023-09-11 20:44:36 +0200

    Create comparison.py

---
+++ comparison.py
@@ -0,0 +1,4 @@
+'''
+Program compares a neuralODE-based model with the NN-interpolator-based model at the task of learning to predict EF from a dataset of patients with some input values and labels EF.
+The program is a synthetic analogy of the Echo video -> EF problem in order to compare the performance of both models.
+'''
